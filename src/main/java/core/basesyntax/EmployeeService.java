bfc76f610abc6f51d11208f5602a5fda6ab754ed
Brynkiewicz <michal.brynkiewicz@envistaco.com>
2026-04-25 21:00:50 +0200

    method name change

--- src/main/java/core/basesyntax/EmployeeService.java
+++ src/main/java/core/basesyntax/EmployeeService.java
@@ -6,7 +6,7 @@ import java.util.Set;
 import java.util.TreeSet;
 
 public class EmployeeService {
-    public Set<Employee> getEmployByOrder(List<Employee> employees) {
+    public Set<Employee> getEmployeeByOrder(List<Employee> employees) {
         Comparator<Employee> employeeComparator = Comparator
                 .comparing(Employee::getAge)
                 .thenComparing(Employee::getName);
