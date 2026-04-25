6dcd61b18913947d7390d2012a6cc33febb2bf91
Brynkiewicz <michal.brynkiewicz@envistaco.com>
2026-04-25 20:58:18 +0200

    first solution

--- src/main/java/core/basesyntax/EmployeeService.java
+++ src/main/java/core/basesyntax/EmployeeService.java
@@ -1,10 +1,19 @@
 package core.basesyntax;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class EmployeeService {
     public Set<Employee> getEmployByOrder(List<Employee> employees) {
-        return null;
+        Comparator<Employee> employeeComparator = Comparator
+                .comparing(Employee::getAge)
+                .thenComparing(Employee::getName);
+
+        TreeSet<Employee> sortedSet = new TreeSet<>(employeeComparator);
+        sortedSet.addAll(employees);
+        return sortedSet;
     }
 }
+
